832e4f62e4021622b7acf7584b345969b5975d7a
clausou <clausou@hotmail.com>
2020-09-02 20:43:30 -0300

    Criação do teclado calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <calculadora msg="Olá! Tudo bem"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import calculadora from './components/calculadora.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    calculadora
   }
 }
 </script>
---
+++ src/components/calculadora.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="calculadora">
+    <div class="display">| 0 |</div>
+    <div>C</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>÷</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>x</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <!-- <div>±</div> -->
+    <div>0</div>
+    <div>,</div>
+    <div>=</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .calculadora {
+    font-size: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+  }
+
+  .display {
+    grid-column: 1 / 5;
+    background-color: #333;
+    color: white;
+  }
+
+</style>
