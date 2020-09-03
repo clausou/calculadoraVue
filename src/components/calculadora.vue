eaf7048d1d0b3f3cf7256dbebfad74be52323d32
clausou <clausou@hotmail.com>
2020-09-02 21:01:11 -0300

    adicionando estilo ao teclado

--- src/components/calculadora.vue
+++ src/components/calculadora.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="calculadora">
-    <div class="display">| 0 |</div>
-    <div>C</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>÷</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>x</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
-    <!-- <div>±</div> -->
-    <div>0</div>
-    <div>,</div>
-    <div>=</div>
+    <div class="display">0</div>
+    <div class="botao">C</div>
+    <div class="botao">+/-</div>
+    <div class="botao">%</div>
+    <div class="botao operadores">÷</div>
+    <div class="botao">7</div>
+    <div class="botao">8</div>
+    <div class="botao">9</div>
+    <div class="botao operadores">x</div>
+    <div class="botao">4</div>
+    <div class="botao">5</div>
+    <div class="botao">6</div>
+    <div class="botao operadores">-</div>
+    <div class="botao">1</div>
+    <div class="botao">2</div>
+    <div class="botao">3</div>
+    <div class="botao operadores">+</div>
+    <div class="botao">±</div>
+    <div class="botao">0</div>
+    <div class="botao">,</div>
+    <div class="botao operadores">=</div>
   </div>
 </template>
 
@@ -33,6 +33,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .calculadora {
+    margin: 0 auto;
+    width: 450px;
     font-size: 40px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
@@ -44,5 +46,16 @@ export default {
     background-color: #333;
     color: white;
   }
+
+  .botao {
+    background-color: #f2f2f2;
+    border: 1px solid rgb(212, 208, 208);
+  }
+
+  .operadores {
+    background-color: rgba(243, 63, 9, 0.794);
+    color: white;
+  }
+
 
 </style>
